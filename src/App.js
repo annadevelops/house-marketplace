d727b050e385cf00bfad1b36ecab9c878a0456ba
Anna N <info@annadevelops.com>
2022-04-05 10:04:27 +0100

    Add skeleton pages to app. Set up routes. Set up navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import index from './index.css'
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-     My App
+  return ( <
+    div className = "App" >
+    <Router >
+    <Routes >
+      <Route path='/' element={<Explore/>}/>
+      <Route path='sign-in' element={<SignIn/>}/>
+      <Route path='sign-up' element={<SignUp/>}/>
+      <Route path='offers' element={<Offers/>}/>
+      <Route path='profile' element={<Profile/>}/>
+      <Route path='forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/*' element={<SignIn/>}/>
+    </Routes>
+    <NavBar />
+    </Router> 
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,77 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+
+function NavBar() {
+
+//initialise the hooks then for item we want a href just use navigate('/pathname') in a onClick function of the element
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  //function to check if the parameter route put in the function is === to location.pathname aka the current path so '/' or '/signin' then call this function in the nav item below to check if currently on the path then change colour of that nav item
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true
+    }
+  }
+
+  return (
+    <footer className='navbar'>
+      <nav className='navbarNav'>
+        <ul className='navbarListItems'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
+            <ExploreIcon
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Explore
+            </p>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
+            <OfferIcon
+              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/offer')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Offers
+            </p>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
+            <PersonOutlineIcon
+              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/profile')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Profile
+            </p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+export default NavBar
