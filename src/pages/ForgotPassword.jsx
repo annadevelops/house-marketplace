26225c5a2c814fdcec862abacbbc56c1046016b2
Anna N <info@annadevelops.com>
2022-04-05 15:51:45 +0100

    Add function to send reset password link to email and style reset password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,60 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+
 function ForgotPassword() {
+  const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+   
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast('A link to reset the password has been sent to your email.')
+      navigate('/sign-in')
+      
+    } catch (error) {
+      toast('Email address does not exist')
+    }
+  }
+
+  const handleChange = (e) => {
+    setEmail(e.target.value)
+  }
+
   return (
-    <div>ForgotPassword</div>
+    <div className='pageContainer'>
+      <header>
+        <p className='pageHeader'>Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={handleChange}
+          />
+          <Link className='forgotPasswordLink' to='/sign-in'>
+            Sign In
+          </Link>
+
+          <div className='signInBar'>
+            <div className='signInText'>Send Reset Link</div>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
   )
 }
 export default ForgotPassword
