4279025ebfa24c7c340e9f136f673a3fecd8c1c2
Anna N <info@annadevelops.com>
2022-04-05 13:49:24 +0100

    Add function to edit personal details on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,56 @@
-import {getAuth} from 'firebase/auth'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {updateDoc, doc} from 'firebase/firestore'
+import {getAuth, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config.js'
+import { toast } from 'react-toastify'
 
 function Profile() {
-  const [user, setUser] = useState(null)
+  const auth = getAuth()
   const navigate = useNavigate()
 
-  //Initialise auth then setUser to the current signed in user, if not signed in return null
-  const auth = getAuth()
+  const [user, setUser] = useState(null)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+  const [changeDetails, setChangeDetails] = useState(false)
 
+  const {name, email} = formData
+
+  
   //logs out user
   const handleLogout = () => {
     auth.signOut()
     navigate('/')
+  }
+
+  //update the display name and name in firestore when 'done' editting
+  const handleUpdate = async () => {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        //update firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        //update firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name
+        })
+      }
+    } catch (error) {
+      toast.error('Cannot change detail(s). Please try again!')
+    }
+  }
+
+  //handle changes to input i.e. name in personal details
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
   }
 
   return (
@@ -21,6 +59,27 @@ function Profile() {
         <p className="pageHeader">My Profile</p>
         <button className="logOut" type='button' onClick={handleLogout}>Logout</button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p className="changePersonalDetails" onClick={() => {
+            changeDetails && handleUpdate() 
+            setChangeDetails((prevState) => !prevState)}}>
+              {changeDetails ? 'done' : 'change'}</p>
+        </div>
+        <div className="profileCard">
+          <form action="">
+            <input type="text" id='name' className={!changeDetails ? 'profileName' : 'profileNameActive'} 
+            disabled={!changeDetails}
+            value={name}
+            onChange={handleChange}/>
+            <input type="text" id='email' className='profileEmail'
+            disabled='true'
+            value={email}
+            onChange={handleChange}/>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
